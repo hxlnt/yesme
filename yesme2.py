d01a2fd8d86424036ddd09c393240c3e3f426b42
Rachel Weil <partytimehexcellent@gmail.com>
2019-01-13 01:30:53 -0600

    Fixing indents

--- yesme2.py
+++ yesme2.py
@@ -37,16 +37,15 @@ def date(msg):
     return msg.date_sent
 recentmsgs = client.messages.list()
 recentmsgs.sort(key=date, reverse=True)
-currentimage = "none"
 for msg in recentmsgs:
     if msg.sid[:1] == "M" and msg.to == TWILIO_PHONE_NUMBER:
         message = client.messages(msg.sid).fetch()
         print(message)
-        allimgs = client.messages(message.sid).media.list()
-        for img in allimgs:
-            imageuri = (TWILIO_BASE_URI + str(img.uri)).strip(".json'").strip("u'")
-        print(imageuri)
         break
+allimgs = client.messages(message.sid).media.list()
+for img in allimgs:
+    imageuri = (TWILIO_BASE_URI + str(img.uri)).strip(".json'").strip("u'")
+    print(imageuri)
 try:
     response = requests.get(imageuri, stream=True).raw
 except requests.exceptions.RequestException as e:  
